b09579ff8a39c37aff0d45289a4fa3272816a03b
Leegongseok <seok20108014@gmail.com>
2024-06-20 17:47:10 +0900

    투표후 결과화면 표시(완성)

--- app/src/main/java/kr/ac/kopo/ex10_2_2024/ResultActivity.kt
+++ app/src/main/java/kr/ac/kopo/ex10_2_2024/ResultActivity.kt
@@ -10,28 +10,27 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        var intent=intent
+        var intent = intent
 
-        var imgName=intent.getStringArrayExtra("imageName");
-        var voteCount=intent.getStringArrayExtra("voteCount");
+        var imgName = intent.getStringArrayExtra("imgName");
+        var voteCount = intent.getIntArrayExtra("voteCount");
 
-        var text= arrayOfNulls<TextView>(imgName!!.size)
-        var ratingBar= arrayOfNulls<RatingBar>(imgName!!.size)
+        var text = arrayOfNulls<TextView>(imgName!!.size)
+        var ratingBar = arrayOfNulls<RatingBar>(imgName!!.size)
 
-        var textId= arrayOf(R.id.text01,R.id.text02,R.id.text03,R.id.text04,R.id.text05,R.id.text06,R.id.text07,R.id.text08,R.id.text09)
-        var ratingBarId= arrayOf(R.id.r1,R.id.r2,R.id.r3,R.id.r4,R.id.r5,R.id.r6,R.id.r7,R.id.r8,R.id.r9)
+        var textId = arrayOf(R.id.text01, R.id.text02, R.id.text03, R.id.text04, R.id.text05, R.id.text06, R.id.text07, R.id.text08, R.id.text09)
+        var ratingBarId = arrayOf(R.id.r1, R.id.r2, R.id.r3, R.id.r4, R.id.r5, R.id.r6, R.id.r7, R.id.r8, R.id.r9)
 
-        for(i in textId!!.indices){
-            text[i]=findViewById<TextView>(textId[i])
-            ratingBar[i]=findViewById<RatingBar>(ratingBarId[i])
+        for (i in textId!!.indices){
+            text[i] = findViewById<TextView>(textId[i])
+            ratingBar[i] = findViewById<RatingBar>(ratingBarId[i])
             text[i]!!.setText(imgName[i])
-            ratingBar[i]!!.rating=voteCount!![i].toFloat()
+            ratingBar[i]!!.rating = voteCount!![i].toFloat()
         }
 
-        var btnHome=findViewById<Button>(R.id.btnHome)
+        var btnHome = findViewById<Button>(R.id.btnHome)
         btnHome.setOnClickListener {
             finish()
         }
-
     }
 }
